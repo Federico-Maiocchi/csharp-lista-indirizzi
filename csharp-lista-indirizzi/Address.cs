a4e92abfdf89e5a38c6ff091ff79eea55be89eb5
Federico-Maiocchi <maiocchifederico@gmail.com>
2024-04-29 16:44:40 +0200

    ADD: try/catch

---
+++ csharp-lista-indirizzi/Address.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Address
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string Province { get; set; }
+
+        public int Zip { get; set; }
+
+        //Costruttore
+        public Address(string name, string surname, string street, string city, string province, int zip ) 
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.Zip = zip;
+        }
+
+        public override string ToString()
+        {
+            return $"l'utente {Name} {Surname} abita in via {Street} nella città {City} ({Province}) cap: {Zip}";
+        }
+    }
+}
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -8,19 +8,63 @@ namespace csharp_lista_indirizzi
         static void Main(string[] args)
         {
 
-        //Buongiorno a tutti,
-        //nome progetto: **csharp - lista - indirizzi * *
-        //nome repo: **csharp - lista - indirizzi * *
-        //Oggi esercitazione sui file, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe,
-        //in particolare nel live - coding di oggi.
-        //In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe,
-        //e salvare tutti gli indirizzi in esso contenuti all’interno di una lista di oggetti istanziati
-        //a partire dalla classe Indirizzo.
+            //nome progetto: **csharp - lista - indirizzi * *
+            //nome repo: **csharp - lista - indirizzi * *
+            //Oggi esercitazione sui file, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe,
+            //in particolare nel live - coding di oggi.
+            //In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe,
+            //e salvare tutti gli indirizzi in esso contenuti all’interno di una lista di oggetti istanziati
+            //a partire dalla classe Indirizzo.
+            //**Attenzione:**gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere 
+            //di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
 
-        //**Attenzione:**gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere 
-        //di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
+            //----------------------------------------------------------------------------------
 
-        //Bonus: iterare la lista di indirizzi e risalvarli in un file.
+            List<Address> addresses = new List<Address>();  
+
+            //Definiamo una stringa per importare il file
+            string path = "C:\\Users\\Federico\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\addresses.csv";
+
+            //Lettura da oggetto a testo
+            var text = File.OpenText(path);
+
+            int i = 0;
+            while (text.EndOfStream == false)
+            {
+                //Console.WriteLine($"Sono in posizione {text.BaseStream.Position}/{text.BaseStream.Length}");
+
+                var line = text.ReadLine();
+                //Console.WriteLine(line);
+
+                i++;
+                if (i <= 1) //ignoro la prima riga
+                    continue;
+
+                try
+                {
+                    //ogni linea corristoponde ad un indirizzo
+                    var data = line.Split(',');
+                    string name = data[0];
+                    string surname = data[1];
+                    string street = data[2];
+                    string city = data[3];
+                    string province = data[4];
+                    int zip = int.Parse(data[5]);
+
+                    Address a = new Address(name, surname, street, city, province, zip);
+                    addresses.Add(a);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+               
+            }
+
+            foreach (var address in addresses)
+                Console.WriteLine(address.ToString());
+
+            //Bonus: iterare la lista di indirizzi e risalvarli in un file.
 
         }
     }
